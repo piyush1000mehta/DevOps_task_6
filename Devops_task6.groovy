5340846955ce32dbe0298c56e523a5786a587249
piyush1000mehta <piyushmehta9909@gmail.com>
2020-08-09 23:44:48 +0530

    final

--- Devops_task6.groovy
+++ Devops_task6.groovy
@@ -1,47 +1,47 @@
-job('job2_deploy6'){
+job('job2_deploy.'){
 description('this job will deploy code')
 triggers {
         upstream('seed_job', 'SUCCESS')
     }
 
 steps {
-shell('''if ! sudo kubectl get pvc | grep php-deploy-pvc
+shell('''if ! kubectl get pvc | grep php-deploy-pvc
          then
-          sudo kubectl create -f /root/.jenkins/workspace/seed_job/php_deploy_pvc.yml
+          kubectl create -f /root/.jenkins/workspace/seed_job/php_deploy_pvc.yml
          fi
 
          if ls /root/.jenkins/workspace/seed_job/ | grep .php
          then
-          if sudo kubectl get deploy | grep php-deploy
+          if kubectl get deploy | grep php-deploy
           then 
-           PODS=$(sudo kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
+           PODS=$(kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
            for i in $PODS
             do 
-             sudo kubectl cp /root/.jenkins/workspace/seed_job/*.php $i:/var/www/html/
+             kubectl cp /root/.jenkins/workspace/seed_job/*.php $i:/var/www/html/
             done
           else
-           sudo kubectl  create -f /root/.jenkins/workspace/seed_job/php_deploy.yml
+           kubectl  create -f /root/.jenkins/workspace/seed_job/php_deploy.yml
            sleep 25
            PODS=$(sudo kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
            for i in $PODS
             do 
-             sudo kubectl cp /root/.jenkins/workspace/seed_job/*.php $i:/var/www/html/
+             kubectl cp /root/.jenkins/workspace/seed_job/*.php $i:/var/www/html/
             done
           fi 
          fi  
 
-         if ! sudo kubectl get svc | grep php-deploy-svc
+         if ! kubectl get svc | grep php-deploy-svc
          then
-          sudo kubectl create -f /root/.jenkins/workspace/seed_job/php_deploy_svc.yml
+          kubectl create -f /root/.jenkins/workspace/seed_job/php_deploy_svc.yml
          fi''') 
 }
 } 
 
 
-job('job3_test6'){
+job('job3_testing'){
 description('this job is for testing' )
 triggers {
-        upstream('job2_deploy6', 'SUCCESS')
+        upstream('job2_deploy.', 'SUCCESS')
     }
 steps{
 shell('''nodeport=$(sudo kubectl get svc -o jsonpath={.items[*].spec.ports[*].nodePort})
@@ -67,7 +67,7 @@ publishers {
 }
 
 
-job(' job4_notify_email6'){
+job(' job4_notify_email'){
 description(' this job will send email to developers ')
  publishers {
         extendedEmail {
